0806f78ce69f9a0ddfbacd7683dcae2f082768aa
rayhan_abdullha <captainrayhan215@gmail.com>
2026-06-15 09:57:43 +0600

    refector: retrive data

--- solution.sql
+++ solution.sql
@@ -2,14 +2,14 @@
     Query 1: Retrieve all upcoming football matches belonging to the 'Champions League'
     where the match status is 'Available'.
 */
-SELECT * FROM Matches
+SELECT match_id, fixture, base_ticket_price FROM Matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
 
 /*
 Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 */
-SELECT * FROM Users
+SELECT user_id, full_name, email FROM Users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
 
@@ -19,9 +19,7 @@ WHERE full_name ILIKE 'Tanvir%'
 SELECT 
     booking_id, 
     user_id, 
-    match_id, 
-    seat_number, 
-    COALESCE(payment_status, 'Action Required') as payment_status, 
-    total_cost
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM Bookings
-WHERE payment_status is NULL;
+WHERE payment_status IS NULL;
